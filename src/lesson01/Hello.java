3a184c0f38caee606ea9779365c4d6248a805dbc
Ilia Tkachov <i.o.tkachov@gmail.com>
2024-10-19 11:22:49 +0300

    add Hello.java

---
+++ src/lesson01/Hello.java
@@ -0,0 +1,11 @@
+package lesson01;
+
+public class Hello {
+    // classpath
+    // lesson01.Hello
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
